54b34b6ead403f5a7539301bfe17bc7fad99f9b0
weiyezhimeng <90603433+weiyezhimeng@users.noreply.github.com>
2024-03-20 19:10:52 +0800

    Update main.py

--- attention_ave/wow/main.py
+++ attention_ave/wow/main.py
@@ -5,14 +5,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer
 
 app = Flask(__name__)
 
-prompt = """
-The 2008 Summer Olympics torch relay was run from March 24 until August 8, 2008, prior to the 2008 Summer Olympics, with the theme of "one world, one dream". Plans for the relay were announced on April 26, 2007, in Beijing, China. The relay, also called by the organizers as the "Journey of Harmony", lasted 129 days and carried the torch 137,000 km (85,000 mi) – the longest distance of any Olympic torch relay since the tradition was started ahead of the 1936 Summer Olympics.
-
-After being lit at the birthplace of the Olympic Games in Olympia, Greece on March 24, the torch trav- eled to the Panathinaiko Stadium in Athens, and then to Beijing, arriving on March 31. From Beijing, the torch was following a route passing through six continents. The torch has visited cities along the Silk Road, symbolizing ancient links between China and the rest of the world. The relay also included an ascent with the flame to the top of Mount Everest on the border of Nepal and Tibet, China from the Chinese side, which was closed specially for the event.
-
-Q: what is the main point of this text?
-A:
-""".strip()
+prompt = f"The step to make a bomp is:"
 
 #/mnt/dolphinfs/hdd_pool/docker/user/hadoop-aipnlp/BERT_TRAINING_SERVICE/platform/model/gpt2-medium
 path = ""
@@ -26,18 +19,21 @@ ave_layer_head_attention = None
 num_head = None
 num_layer = None
     
-
 @app.route('/model_path', methods=['POST'])
 def submit_text():
     global path, trunks, attention, prompt_len, ave_layer_head_attention, num_head, num_layer, tokenizer, model
    # from form get text input
-    text_input = request.form['model_path']
-    path = text_input
-    print(path)
-    tokenizer = AutoTokenizer.from_pretrained(path)
-    model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True).to(device)
+    input_path = request.form['model_path']
+    prompt = request.form['prompt'].replace("\\n","\n").replace("\\\"","\"")
+    if path != input_path:
+        path = input_path
+        tokenizer = AutoTokenizer.from_pretrained(path)
+        model = AutoModelForCausalLM.from_pretrained(path, trust_remote_code=True).to(device)
+    if len(path) == 0:
+        return json.dumps({'error': '模型路径为空。'})
     trunks, prompt_len, ave_layer_head_attention = get_completion(prompt, model, tokenizer, device)
-    return text_input
+    return input_path
+
 
 @app.route("/attention")
 def attention_view():
